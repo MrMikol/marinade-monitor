2fcfd09bada0a2fdf4a5c3dc28175383f5b8a7bb
Michael Tan <michaelneiltan04@gmail.com>
2026-05-11 10:05:56 -0400

    API connectivity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // let url = "https://validator-bonds-api.marinade.finance/docs.json";
+    let url = "https://validator-bonds-api.marinade.finance/bonds/bidding";
+    // let body = reqwest::get(url).await?.text().await?;
+    
+    let json: serde_json::Value = reqwest::get(url).await?.json().await?;
+    
+    println!("{:#}", json);
+    
+    Ok(())
 }
